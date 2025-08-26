9611c9df943151391f4ed34b0386f969a55cae3b
Ann Nyaga <137498254+Nyaga-Ann@users.noreply.github.com>
2025-08-26 07:22:37 +0300

    added custom USER MODEL WITH ROLES AND ADMIN REGISTRATION

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        BUYER = "buyer", "Buyer"
+        SELLER = "seller", "Seller"
+
+    role = models.CharField(max_length=10, choices=Roles.choices, default=Roles.BUYER)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
