877a0dbfa5aa6b810441294a6701047d497e649e
Ann Nyaga <137498254+Nyaga-Ann@users.noreply.github.com>
2025-08-26 07:35:04 +0300

    added item listing model and admin with useful filters and search

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ItemListing
 
-# Register your models here.
+@admin.register(ItemListing)
+class ItemListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "location", "vendor", "created_at")
+    search_fields = ("name", "category", "location", "vendor__business_name")
+    list_filter = ("category",)
