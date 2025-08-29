b6b3a881cbcc4597a6e065c15242a6a7b70aad27
Ann Nyaga <137498254+Nyaga-Ann@users.noreply.github.com>
2025-08-29 14:12:55 +0300

    added WhatsApp contact link to vendor serializer

--- vendors/serializers.py
+++ vendors/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import VendorProfile
 
-class VendorSerializer(serializers.ModelSerializer):
+class VendorProfileSerializer(serializers.ModelSerializer):
+    whatsapp_link = serializers.SerializerMethodField()
+
     class Meta:
         model = VendorProfile
-        fields = "__all__"
-        read_only_fields = ["user"]
+        fields = [
+            "id", "business_name", "location", "latitude", "longitude",
+            "description", "contact_phone", "shop_type",
+            "whatsapp_number", "whatsapp_link"
+        ]
+
+    def get_whatsapp_link(self, obj):
+        if obj.whatsapp_number:
+            return f"https://wa.me/{obj.whatsapp_number}"
+        return None
